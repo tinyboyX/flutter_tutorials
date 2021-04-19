3385dac2f3d05ed53e79e953d67ade7b5187b964
tinyboyx <phamgiang2811@gmail.com>
2021-04-19 15:47:58 +0700

    add package and modify time with intl

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,6 @@ import 'MyApp.dart';
 void main(){
   //Center is a widget, Text is a widget
   runApp(
-      MyApp(name: 'Giang',age: 23)//How to send arguments/params to this Widget ?
+      MyApp()//How to send arguments/params to this Widget ?
   );
 }
