21edcc406f7cc896a3b96493f43d8d1af1d08c4d
tinyboyx <phamgiang2811@gmail.com>
2021-05-10 16:03:05 +0700

    Update UI

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'transaction.dart';
 
 //You can define your own Widget
 class MyApp extends StatefulWidget {
@@ -12,8 +13,103 @@ class MyApp extends StatefulWidget {
 
 //This is a "very basic" statefulwidget
 class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  final _contentController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  //define states
+  String _content;
+  double _amount;
+  Transaction _transaction = Transaction('', 0.0);
+
+  List<Transaction> _transactions = <Transaction>[];
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+  }
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MaterialApp(
+        title: "This is a StatefulWidget",
+        home: Scaffold(
+            key: _scaffoldKey,
+            body: SafeArea(
+                minimum: const EdgeInsets.only(left: 20, right: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Content'),
+                      controller: _contentController,
+                      onChanged: (text) {
+                        setState(() {
+                          _transaction.content = text ?? null;
+                        });
+                      },
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Amount(money)'),
+                      controller: _amountController,
+                      onChanged: (text) {
+                        setState(() {
+                          _transaction.amount =
+                              double.tryParse(text) ?? 0; //if error, value = 0
+                        });
+                      },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                    ),
+                    ButtonTheme(
+                        height: 60,
+                        // ignore: deprecated_member_use
+                        child: FlatButton(
+                          child: Text(
+                            'Insert Transaction',
+                            style: const TextStyle(fontSize: 18),
+                          ),
+                          color: Colors.pinkAccent,
+                          textColor: Colors.white,
+                          onPressed: () {
+                            setState(() {
+                              _transactions.add(_transaction);
+                              // clear content in text field
+                              _transaction = Transaction('', 0.0);
+                              _contentController.text = '';
+                              _amountController.text = '';
+                            });
+                            //print('Content = $_content, money\'s amount = $_amount');
+                            // Display list
+                            //ignore: deprecated_member_use
+                            _scaffoldKey.currentState.showSnackBar(SnackBar(
+                              content: Text('Transaction List :' +
+                                  _transactions.toString()),
+                              duration: Duration(seconds: 3),
+                            ));
+                          },
+                        )),
+                    Column(
+                      children: <Widget>[
+                        ListTile(
+                          leading: const Icon(Icons.airline_seat_flat_sharp),
+                          title: const Text("Test"),
+                          subtitle: const Text("data"),
+                          onTap: () {
+                            print("Taped");
+                          },
+                        )
+                      ],
+                    )
+                  ],
+                ))));
   }
 }
---
+++ lib/transaction.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Transaction {
+  String content;
+  double amount;
+
+  //constructor
+  Transaction(this.content, this.amount);
+
+  @override
+  String toString() {
+    return 'content = $content  and amount = $amount';
+  }
+}
